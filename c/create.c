520a79f5b6fe528fa2004056cb7835766e3222ef
Isha Isha <n1v7@deas.ugrad.cs.ubc.ca>
2013-10-03 22:42:49 -0700

    PCB definition

--- c/create.c
+++ c/create.c
@@ -2,5 +2,21 @@
  */
 
 #include <xeroskernel.h>
+typedef enum {
+  READY,
+  BLOCKED,
+  STOPPED,
+  RUNNING
+} ProcessState;
+
+typedef struct {
+  int pid;
+  ProcessState state;
+  int priority;
+  struct PCB * next;
+} PCB;
+
+extern PCB * ReadyQueue;
+extern PCB * Running;
 
 /* Your code goes here. */
