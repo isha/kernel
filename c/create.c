8089726ac18181ebe0d3749c0b00d52c481972ad
Isha Isha <n1v7@deas.ugrad.cs.ubc.ca>
2013-10-09 15:16:17 -0700

    Added process cleanup

--- c/create.c
+++ c/create.c
@@ -7,7 +7,7 @@ static int NextPID = 1;
 
 extern int create (void (*func)(), int stack) {
   // Allocate large enough memory for process
-  long size = sizeof(PCB)+sizeof(ContextFrame)+stack*2;
+  long size = sizeof(PCB)+sizeof(ContextFrame)+stack+16; // Extra 16 just in case
   PCB * pcb = kmalloc(size);
 
   if (pcb != NULL) {
@@ -30,4 +30,5 @@ extern int create (void (*func)(), int stack) {
 }
 
 extern void cleanup (PCB * pcb) {
+  kfree(pcb);
 }
